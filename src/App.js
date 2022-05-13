1c42dc28a3fdcfa19ad77f62c260968033064b62
ioanat94 <ioanatiplea94@gmail.com>
2022-05-13 13:42:21 +0300

    Added ability to search location

--- src/App.js
+++ src/App.js
@@ -2,36 +2,60 @@ import React, {useState} from 'react'
 import axios from 'axios'
 
 function App() {
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=helsinki&appid=762b20cd9002fa00ebc594f2b95172ec&units=metric`
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=762b20cd9002fa00ebc594f2b95172ec&units=metric`
+
+  function searchLocation(event) {
+    if (event.key === 'Enter') {
+      axios.get(url).then(response => {
+        setData(response.data)
+      })
+
+      setLocation('')
+    }
+  }
 
   return (
-    <div className="app">
-      <div className="app--ctr">
-        <div className="app--ctr-top">
-          <div className="app--ctr-top-loc">
-            <p>Helsinki</p>
+    <div className='app'>
+      <div className='app--search'>
+        <input 
+          type='text' 
+          value={location} 
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter location'
+        />
+      </div>
+      <div className='app--ctr'>
+        <div className='app--ctr-top'>
+          <div className='app--ctr-top-loc'>
+            <p>{data.name}</p>
           </div>
-          <div className="app--ctr-top-temp">
-            <p>14°C</p>
+          <div className='app--ctr-top-temp'>
+            {data.main ? <p>{Math.round(data.main.temp)}°C</p> : null}
           </div>
-          <div className="app--ctr-top-desc">
-            <p>Clouds</p>
-          </div>
-        </div>
-        <div className="app--ctr-bot">
-          <div className="app--ctr-bot-feels">
-            <p className="value">12°C</p>
-            <p>Feels Like</p>
-          </div>
-          <div className="app--ctr-bot-hum">
-            <p className="value">20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="app--ctr-bot-wind">
-            <p className="value">8 km/h</p>
-            <p>Wind Speed</p>
+          <div className='app--ctr-top-desc'>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
+        {data.name != undefined &&
+          <div className='app--ctr-bot'>
+            <div className='app--ctr-bot-feels'>
+              {data.main ? <p className='value'>{Math.round(data.main.feels_like)}°C</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className='app--ctr-bot-hum'>
+              {data.main ? <p className='value'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className='app--ctr-bot-wind'>
+              {data.wind ? <p className='value'>{Math.round(data.wind.speed)} km/h</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
       </div>
     </div>
   );
